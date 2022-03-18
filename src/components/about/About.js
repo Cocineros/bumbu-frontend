8c19a60605023c9299ddc7a4b390ec38407e8251
amadeamargo <amadeamargo@gmail.com>
2022-03-17 23:32:23 -0500

    made changes to make design responsive

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,10 +2,12 @@ import './about.css'
 
 export default function About() {
     return (
-        <div className="about-container">
-                        <h1>about</h1>
+        // <div className="about-container">
+        //                 <h1>about</h1>
          
 
-        </div>
+        // </div>
+        <>
+        </>
     )
 }
--- src/components/testimonal/Testimonial.js
+++ src/components/testimonal/Testimonial.js
@@ -1,37 +1,3 @@
-// import './testimonial.css'
-// import { Avatar, Image, Card } from 'antd';
-// import { EditOutlined, EllipsisOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
-// const { Meta } = Card;
-
-
-// export default function Testimonial() {
-//     return (
-//         <div className="testimonial-container">
-//             <h1>user stories</h1>
-//             <div className='test-containers'>
-//             <div className="left-test" id="test1">
-//                 <Avatar className='avatar' size={64} icon={<UserOutlined />} />
-
-//                 <h2>"I wanted an app that kept all of my recipes for me so that I can easily share them with friends and family."</h2>
-
-//             </div>
-//             <br></br>
-//             <div className ="right-test" id="test2">
-//                 <Avatar className='avatar' size={64} icon={<UserOutlined />} />
-
-//                 <h2>"I wanted my recipes to look nice with minimal effort."</h2>
-//             </div>
-//             <br></br>
-//             <div className="left-test" id="test3">
-//                 <Avatar className='avatar' size={64} icon={<UserOutlined />} />
-
-//                 <h2>"I wanted to have access to all my recipes, so I can use them whether I am traveling or at home."</h2>
-//             </div>
-//             </div>
-//         </div>
-//     );
-// }
-
 import './testimonial.css'
 import { Avatar, Image, Card } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
@@ -46,20 +12,23 @@ export default function Testimonial() {
             </div>
             <div className="parent-container">
                 <div className="left-column">
-                    <div id="test1">
+                    <div className="test-containers" id="test1-container">
                         <Avatar className='avatar' size={64} icon={<UserOutlined />} />
+                        <div className="test-text">
                         <h2>"I wanted an app that kept all of my recipes for me so that I can easily share them with friends and family."</h2>
+                        </div>
                     </div>
-                    <div id="test3">
+                    <div className="test-containers" id="test2-container">
                         <Avatar className='avatar' size={64} icon={<UserOutlined />} />
-
+                        <div className="test-text">
                         <h2>"I wanted to have access to all my recipes, so I can use them whether I am traveling or at home."</h2>
+                        </div>
                     </div>
                 </div>
                 <div className="right-column">
-                    <div id="test2-container">
-                        <div id="test2-text">
+                    <div className="test-containers" id="test3-container">
                             <Avatar className='avatar' size={64} icon={<UserOutlined />} />
+                            <div className="test-text">
                             <h2>"I wanted my recipes to look nice with minimal effort."</h2>
                         </div>
                     </div>
