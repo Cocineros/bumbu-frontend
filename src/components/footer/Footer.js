8d603b744386548954d4e151a4e5e87f84cc746b
Janette <janette.castillo91@yahoo.com>
2022-03-19 11:49:10 -0500

    formatted testimonials

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,8 +4,12 @@ import { GithubOutlined } from '@ant-design/icons';
 export default function Footer() {
     return (
         <div className="footer-container">
-            <a href= "https://github.com/Cocineros"> 
+             <h3>Bumbu © 2022</h3>
+        <a href= "https://github.com/Cocineros"> 
             <GithubOutlined /> </a>
+            <p>thank you for supporting Bumbu and our vision
+
+                </p>
         </div>
     )
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -18,50 +18,42 @@ export default function Home() {
     event.preventDefault();
     Auth.logout();
   };
-    
-    return (
-        <div>
-          {Auth.loggedIn() ? (
-            <>
-            {/* <Dashboard/> */}
-              {/* <Link className="btn btn-lg btn-primary m-2" to="/dashboard"> View My Profile
+
+  return (
+    <div>
+      {Auth.loggedIn() ? (
+        <>
+          {/* <Dashboard/> */}
+          {/* <Link className="btn btn-lg btn-primary m-2" to="/dashboard"> View My Profile
               </Link> 
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button> */}
-              <div class="home components">
-                <Welcome />
-                
-              <About />
-              <Testimonial />
-              <Footer />
-              <BackTop />
-              </div>
-              
-            </>
-          ) : (
-            <>
-             <div class="home-components">
-        <Welcome />
-        
-        <div id="buttons-container">
-        <div id='thank-you'>
-                <p>thank you for supporting Bumbu and our vision
+          <div class="home components">
+            <Welcome />
+            <About />
+            <Testimonial />
+            <Footer />
+            <BackTop />
+          </div>
 
-                </p>
+        </>
+      ) : (
+        <>
+          <div class="home-components">
+            <Welcome />
 
+            <div id="buttons-container">
+              <SignupModal />
+              <LoginModal />
             </div>
-        <SignupModal/>
-        <LoginModal/>
-        </div>
-        <About/>
-        <Testimonial/>
-        <Footer/> 
-        <BackTop />
-        {/* <strong className="site-back-top-basic">  </strong> */}
-        </div>
-            </>
-          )}
-         </div>
+            <About />
+            <Testimonial />
+            <Footer />
+            <BackTop />
+          </div>
+        </>
+      )}
+    </div>
   );
 };
