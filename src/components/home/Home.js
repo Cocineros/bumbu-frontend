32f3b65e0c57ab3340b27a53c67136995ad443c1
Janette <janette.castillo91@yahoo.com>
2022-03-18 19:51:27 -0500

    added logout buttons to navbar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -25,11 +25,11 @@ export default function Home() {
           {Auth.loggedIn() ? (
             <>
             {/* <Dashboard/> */}
-              <Link className="btn btn-lg btn-primary m-2" to="/dashboard"> View My Profile
+              {/* <Link className="btn btn-lg btn-primary m-2" to="/dashboard"> View My Profile
               </Link> 
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
-              </button>
+              </button> */}
               <div class="home components">
                 <Welcome />
                 
