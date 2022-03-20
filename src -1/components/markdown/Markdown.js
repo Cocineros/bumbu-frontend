ca7d45a4c1f74757366fb8ec190511ab233cc7da
Janette <janette.castillo91@yahoo.com>
2022-03-20 16:32:53 -0500

    made homepage responsive

--- src -1/components/markdown/Markdown.js
+++
@@ -1,7 +0,0 @@
-import { Wysimark, useEditor } from '@wysimark/react'
-
-export default function Markdown () {
-  const editor = useEditor({ initialMarkdown: "# Hello World" })
-  return (
-  <Wysimark editor={editor} />)
-}
